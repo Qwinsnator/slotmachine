ff5ab386d1ef5c345640ce3b4f93e1dd0cfd7760
Qwinsnator <quintenman4@gmail.com>
2021-09-24 09:23:57 +0000

    d

---
+++ slot.js
@@ -0,0 +1,18 @@
+class Slot {
+    x;
+    y;
+    nummer;
+
+    constructor(newX, newY, _nummer) {
+        this.x = newX;
+        this.y = newY;
+        this.nummer = _nummer;
+    }
+
+
+    show() {
+        this.nummer
+    }
+
+
+}
